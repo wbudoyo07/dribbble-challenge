9b886666c43c3ef0af34460cd90b01a9a6e15a04
William Budoyo <williambu07@gmail.com>
2019-01-09 13:02:35 -0500

    basic practice flexbox

---
+++ dribbble-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+      <div class="red">1</div>
+      <div class="green">2</div>
+      <div class="blue">3
+          {/* <p>Flexbox items can grow and shrink as required to fit their contents.</p> */}
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
